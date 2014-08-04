350f366a7404e8c3992131f97cf50e6889e90e2d
Zack Botkin <zachary.d.botkin@baml.com>
2014-08-04 13:00:59 -0400

    fixed up the db script slightly

--- bin/db.py
+++ bin/db.py
@@ -1,9 +1,4 @@
-
-
-
 import sqlite3
-
-
 
 from src.adapters import DbAdapter
 
@@ -30,7 +25,8 @@ def create_tasks_db(DB_CONN):
         ['task', 'due_date', 'ttype'],
         ['text', 'text', 'text']
     )
-    
+
+
 def create_wants_db(DB_CONN):
 
     db_adapter = DbAdapter(DB_CONN)
@@ -51,26 +47,25 @@ def create_breaks_db(DB_CONN):
         ['text', 'text']
     )
 
-
-
 def insert_user(DB_CONN, username, password):
 
-    db_adapter = DbAdapter(DB_CONN) 
+    db_adapter = DbAdapter(DB_CONN)
     db_adapter.insert_row('users', [username, password])
-    
+
+
 def insert_task(DB_CONN, task, due_date, ttype):
 
-    db_adapter = DbAdapter(DB_CONN) 
+    db_adapter = DbAdapter(DB_CONN)
     db_adapter.insert_row('tasks', [task, due_date, ttype])
 
 def insert_want(DB_CONN, want, wtype):
 
-    db_adapter = DbAdapter(DB_CONN) 
+    db_adapter = DbAdapter(DB_CONN)
     db_adapter.insert_row('wants', [want, wtype])
 
 def insert_break(DB_CONN, dbreak, link):
 
-    db_adapter = DbAdapter(DB_CONN) 
+    db_adapter = DbAdapter(DB_CONN)
     db_adapter.insert_row('breaks', [dbreak, link])
 
 
@@ -91,19 +86,19 @@ def list_wants(DB_CONN):
     results = db_adapter.get_all('wants')
     for result in results:
         print(result)
-        
+
 def list_breaks(DB_CONN):
     db_adapter = DbAdapter(DB_CONN)
     results = db_adapter.get_all('breaks')
     for result in results:
         print(result)
 
-# SHOULD 
+# SHOULD
 def delete_user(DB_CONN):
     pass
 
 def delete_users_db(DB_CONN):
-    
+
     db_adapter = DbAdapter(DB_CONN)
     db_adapter.delete_table('users')
 
@@ -111,29 +106,29 @@ def delete_task(DB_CONN):
     pass
 
 def delete_tasks_db(DB_CONN):
-    
+
     db_adapter = DbAdapter(DB_CONN)
     db_adapter.delete_table('tasks')
-    
+
 def delete_want(DB_CONN):
     pass
 
 def delete_wants_db(DB_CONN):
-    
+
     db_adapter = DbAdapter(DB_CONN)
     db_adapter.delete_table('wants')
-    
+
 def delete_break(DB_CONN):
     pass
 
 def delete_breaks_db(DB_CONN):
-    
+
     db_adapter = DbAdapter(DB_CONN)
-    db_adapter.delete_table('breaks')    
+    db_adapter.delete_table('breaks')
 
 
 def create_dialog(DB_CONN):
-    
+
     print("Creating a db")
     print("1) users\n2) tasks\n3) wants\n4) breaks")
     input = raw_input()
@@ -143,25 +138,25 @@ def create_dialog(DB_CONN):
             create_users_db(DB_CONN)
         except:
             print('Error. Users db probably already exists')
-    
+
     elif input == str(2) or input == 'tasks':
         try:
             create_tasks_db(DB_CONN)
         except:
             print('Error. Users db probably already exists')
-    
+
     elif input == str(3) or input == 'wants':
         try:
             create_wants_db(DB_CONN)
         except:
             print('Error. Users db probably already exists')
-    
+
     elif input == str(4) or input == 'breaks':
         try:
             create_breaks_db(DB_CONN)
         except:
             print('Error. Users db probably already exists')
-    
+
     else:
         print("Not implemented yet")
 
@@ -182,21 +177,21 @@ def insert_dialog(DB_CONN):
         password = raw_input()
 
         insert_user(DB_CONN, username, password)
-        
-    if input == str(2) or input == 'tasks':
+
+    elif input == str(2) or input == 'tasks':
 
         print("Task?")
         task = raw_input()
 
         print("Due Date mmddyyy?")
         due_date = raw_input()
-        
+
         print("Genre?")
         ttype = raw_input()
 
-        insert_tasks(DB_CONN, task, due_date, ttype)
-        
-    if input == str(3) or input == 'wants':
+        insert_task(DB_CONN, task, due_date, ttype)
+
+    elif input == str(3) or input == 'wants':
 
         print("want?")
         want = raw_input()
@@ -204,9 +199,9 @@ def insert_dialog(DB_CONN):
         print("wtype")
         wtype = raw_input()
 
-        insert_user(DB_CONN, want, wtype)
-        
-    if input == str(4) or input == 'breaks':
+        insert_want(DB_CONN, want, wtype)
+
+    elif input == str(4) or input == 'breaks':
 
         print("break?")
         dbreak = raw_input()
@@ -214,9 +209,10 @@ def insert_dialog(DB_CONN):
         print("link")
         link = raw_input()
 
-        insert_user(DB_CONN, dbreak, link)
+        insert_break(DB_CONN, dbreak, link)
     else:
-        print("not implemented yet")
+        print("Not a valid choice")
+
 
 def list_dialog(DB_CONN):
 
@@ -227,22 +223,22 @@ def list_dialog(DB_CONN):
 
     if input == str(1) or input == 'users':
         list_users(DB_CONN)
-    
+
     elif input == str(2) or input == 'tasks':
         list_tasks(DB_CONN)
-    
+
     elif input == str(3) or input == 'wants':
         list_wants(DB_CONN)
-    
+
     elif input == str(4) or input == 'breaks':
-        list_breaks(DB_CONN)    
-    
+        list_breaks(DB_CONN)
+
     else:
-        print("not implemented yet")
+        print("Not a valid choice")
 
 
 def delete_dialog(DB_CONN):
-    
+
     print("Deleting a db")
     print("1) users\n2) tasks\n3) wants\n4) breaks")
 
@@ -253,26 +249,26 @@ def delete_dialog(DB_CONN):
             delete_users_db(DB_CONN)
         except:
             print('Error. Users db probably does not exist')
-    
+
     if input == str(2) or input == 'tasks':
         try:
             delete_tasks_db(DB_CONN)
         except:
             print('Error. Users db probably does not exist')
-    
+
     if input == str(3) or input == 'wants':
         try:
             delete_wants_db(DB_CONN)
         except:
             print('Error. Users db probably does not exist')
-    
+
     if input == str(4) or input == 'breaks':
         try:
             delete_breaks_db(DB_CONN)
         except:
             print('Error. Users db probably does not exist')
     else:
-        print("not implemented yet")
+        print("Not a valid choice")
 
 
 
@@ -280,15 +276,15 @@ def main():
 
     print("Connecting to %s" % DB_PATH)
 
-    
+
     DB_CONN = sqlite3.connect(DB_PATH)
     print("Connection Successful")
-   
-   
+
+
     session_over = False
 
     while not session_over:
-    
+
         print("1) create\n2) insert\n3) list\n4) delete")
 
         input = raw_input()
@@ -306,7 +302,7 @@ def main():
             session_over = not session_over
 
 
-    
+
 
 main()
 
