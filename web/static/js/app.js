dab7d9531f78f2ab8c40926650f8b95cfb2db770
U-Zackster-PC\Zackster <Zackster@Zackster-PC.(none)>
2014-09-06 00:18:50 -0500

    partial segments working

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -4,26 +4,42 @@ angular.module('myApp', [
     'myApp.controllers',
     'myApp.services',
     'myApp.directives',
-    'ngRoute'
+    'ngRoute',
+    'route-segment',
+    'view-segment'
 ])
-.config(['$routeProvider', function($routeProvider){
+.config(function($routeSegmentProvider, $routeProvider){
 
-    $routeProvider
-    .when('/', {
+    $routeSegmentProvider.
+    
+    when('/home', 's1').
+    when('/schedule', 's2').
+    when('/add', 's3').
+    when('/add/class', 's3.addClass').
+
+    segment('s1', {
         templateUrl: 'static/partials/home.html',
         controller: 'homeCtrl'
-    })
-    .when('/schedule', {
+    }).
+    segment('s2', {
         templateUrl: 'static/partials/schedule.html',
         controller: 'scheduleCtrl'
-    })
-    .when('/add_class', {
-        templateUrl: 'static/partials/add_class.html',
-        controller: 'addClassCtrl'
-    })
-    .otherwise({
-        redirectTo: '/'
+    }).
+    segment('s3', {
+        templateUrl: 'static/partials/add.html'        
+    }).
+    within().
+        segment('addClass', {
+            templateUrl: 'static/partials/add_class.html',
+            controller: 'addClassCtrl'
+        }).
+        up();
+
+    $routeProvider.otherwise({
+        redirectTo: '/home'
     });
 
-}]);
+
+});
+
 
