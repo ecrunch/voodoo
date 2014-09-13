3b3092f645db16a1e64ff979bbec8daeb0f02034
U-Zackster-PC\Zackster <Zackster@Zackster-PC.(none)>
2014-09-12 23:09:23 -0500

    added one test, dan will take over

--- test/task_tests.py
+++ test/task_tests.py
@@ -1,9 +1,16 @@
 
 
 
+#
+#
+#   Tests for task class
+#   currently located at src/classes, about halfway down the file
+#
+#
 
 
 import unittest
+import datetime
 
 from src.classes import Task
 
@@ -12,14 +19,30 @@ from src.classes import Task
 class TestTaskClass(unittest.TestCase):
 
     def setUp(self):
-        self.task_one = Task(user= None)
+         
+        today = datetime.datetime.today()
+        days_away = 11  ## <-- what happens to the score if the days is less than 11....?
+        due_date = today + datetime.timedelta(days= days_away)
+        date_str = datetime.datetime.strftime(due_date, "%Y/%m/%d")
+
+        self.exam_basic = Task(total_minutes = 0, task_type= 'Exam', date_str= date_str)
+        self.paper_basic = Task(total_minutes = 0, task_type= 'Paper', date_str= date_str)
+        self.project_basic = Task(total_minutes = 0, task_type= 'Project', date_str= date_str)
+        self.homework_basic = Task(total_minutes = 0, task_type= 'Homework', date_str= date_str)
 
     def tearDown(self):
-        pass
+        self.exam_one = None  ## <-- resets after every test
+        self.paper_basic = None
+        self.project_basic = None
+        self.homework_basic = None
 
-    # TODO : name something more clever
-    def testOne(self):
-        print(self.task_one)
+
+    # tests the starting scores 
+    def testBaseScores(self):
+        self.assertEqual(self.exam_basic.get_score(), 5)
+        self.assertEqual(self.paper_basic.get_score(), 4)
+        self.assertEqual(self.project_basic.get_score(), 4)
+        self.assertEqual(self.homework_basic.get_score(), 3)
 
 
 
